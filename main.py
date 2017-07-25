3be2a95240dd7bab837622eccef949a1f9c15062
Chong Zhang <zigchg@gmail.com>
2017-07-25 13:26:08 -0400

    Revert "Typecast dict.keys() to list"
    This reverts commit c73d80d8e43cf4a17f3143c5eb4b2c0504f940fa.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import importlib
-
 __author__ = 'infosense'
 import sys,json,datetime,os,re,collections
 from datetime import datetime
@@ -432,7 +430,7 @@ def validate(document, parsed_query,restricted): # Need to write
         return False
 
 if __name__ == "__main__":
-    importlib.reload(sys)
+    reload(sys)
     sys.setdefaultencoding("utf-8")
     query_path = "post_point_fact.json"
     answer_path = "answer.json"
